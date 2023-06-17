b6dc07fd4fd5b5cc7f06abaac52d76f742bbb34f
asheroto <github@asheroto.com>
2023-06-17 15:23:44 -0500

    Major updates

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-
 ![image](https://user-images.githubusercontent.com/49938263/164990481-a82586ac-db45-42b1-b543-c3756eafe045.png)
+
 # Install winget-cli from PowerShell
 
-- Install [winget-cli](https://github.com/microsoft/winget-cli) straight from PowerShell
-- Works on Windows 10, Windows 11, Server 2022
-- Does not work on Server 2019
-- Always gets the latest version of winget
-  
+-   Install [winget-cli](https://github.com/microsoft/winget-cli) straight from PowerShell
+-   Works on Windows 10, Windows 11, Server 2022
+-   Does not work on Server 2019
+-   Always gets the latest version of winget
 
-## How to use 
+## How to use
 
 ### Method 1 - no need to download the file because it connects to PSGallery to fetch it
 
-- In PowerShell, type `Install-Script -Name winget-install` and answer yes to any prompts
+-   In PowerShell, type `Install-Script -Name winget-install` and answer yes to any prompts
 
-- Then type `winget-install`
+-   Then type `winget-install`
 
 ### Tip - how to trust PSGallery
 
@@ -24,52 +23,44 @@ If you want to trust PSGallery so you aren't prompted each time you run this com
 
 ### Method 2 - download file locally and run
 
-- Download `winget-install.ps1` or `winget-install-signed.zip` (and extract the inner PS1 script)
+-   Download `winget-install.ps1` or `winget-install-signed.zip` (and extract the inner PS1 script)
 
-- Run the script with `.\winget-install.ps1`
-
-  
+-   Run the script with `.\winget-install.ps1`
 
 ## Available Scripts
 
--  **winget-install.ps1**
+-   **winget-install.ps1**
 
-	- Unsigned script
+    -   Unsigned script
 
--  **winget-install-signed.zip**
+-   **winget-install-signed.zip**
 
-	- Signed script
+    -   Signed script
 
-	- Compressed for transport and to retain signature
+    -   Compressed for transport and to retain signature
 
-	- Uncompress to use
+    -   Uncompress to use
 
-	- For use if you do not want to enable unsigned script execution in PowerShell
-
-  
+    -   For use if you do not want to enable unsigned script execution in PowerShell
 
 ## Script Functionality
 
-  
+-   Latest version of [Microsoft.UI.Xaml](https://www.nuget.org/packages/Microsoft.UI.Xaml/) is downloaded and installed.
 
-- [VCLibs](https://docs.microsoft.com/en-gb/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages) and [Xaml](https://www.nuget.org/packages/Microsoft.UI.Xaml/) are installed first
+-   [VCLibs](https://docs.microsoft.com/en-gb/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages) and [Xaml](https://www.nuget.org/packages/Microsoft.UI.Xaml/) are installed first
 
-- Processor architecture is determined for prerequisites
+-   Processor architecture is determined for prerequisites
 
-- [VCLibs](https://docs.microsoft.com/en-gb/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages) is installed straight from the appx package
+-   [VCLibs](https://docs.microsoft.com/en-gb/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages) is installed straight from the appx package
 
-- [Xaml](https://www.nuget.org/packages/Microsoft.UI.Xaml/) is installed by downloading the **nupkg**, extracting it, and installing the **appx** package
+-   [Xaml](https://www.nuget.org/packages/Microsoft.UI.Xaml/) is installed by downloading the **nupkg**, extracting it, and installing the **appx** package
 
-- [winget-cli](https://github.com/microsoft/winget-cli) is then installed
+-   [winget-cli](https://github.com/microsoft/winget-cli) is then installed
 
-- User **PATH** variable is adjusted to include WindowsApps folder
+-   User **PATH** variable is adjusted to include WindowsApps folder
 
-- Grabs the latest version of winget on each run
-
-  
+-   Grabs the latest version of winget on each run
 
 ## Note
 
-  
-
-- If you receive an error message about the Appx module not being loaded, try using a different version of PowerShell (version 6 and 7 seem to be buggy still, but the native PowerShell version in Windows works)
+-   If you receive an error message about the Appx module not being loaded, try using a different version of PowerShell (version 6 and 7 seem to be buggy still, but the native PowerShell version in Windows works)
--- winget-install.ps1
+++ winget-install.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 0.0.4
+.VERSION 1.0.0
 
 .GUID 3b581edb-5d90-4fa1-ba15-4f2377275463
 
@@ -17,7 +17,8 @@
 [Version 0.0.2] - Implemented function to get the latest version of Winget and its license.
 [Version 0.0.3] - Signed file for PSGallery.
 [Version 0.0.4] - Changed URI to grab latest release instead of releases and preleases.
-[Version 0.0.4.1] - [additional change] change Microsoft.UI.Xaml to v2.8, download files to download-directory
+[Version 0.0.5] - Update version number of dependencies.
+[Version 1.0.0] - Major refactor code, see release notes for more information.
 
 #>
 
@@ -27,113 +28,126 @@
 .DESCRIPTION
     Downloads the latest version of Winget, its dependencies, and installs everything. PATH variable is adjusted after installation. Reboot required after installation.
 .EXAMPLE
-    winget-install
+    Install-Winget
 .NOTES
-    Version      : 0.0.4.1
+    Version      : 1.0.0
     Created by   : asheroto
 .LINK
     Project Site: https://github.com/asheroto/winget-installer
 #>
 
+# Versions
+$VCLibsVersion = "14.00"
+$ProgressPreference = 'SilentlyContinue' # Suppress progress bar (makes downloading super fast)
 
-function getNewestLink($match) {
-	$uri = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-	Write-Verbose "[$((Get-Date).TimeofDay)] Getting information from $uri"
-	$get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
-	Write-Verbose "[$((Get-Date).TimeofDay)] getting latest release"
-	$data = $get[0].assets | Where-Object name -Match $match
-	return $data.browser_download_url
+# Get latest version of Microsoft.UI.Xaml
+function Get-LatestMicrosoftUIXamlVersion() {
+	$url = "https://api.nuget.org/v3-flatcontainer/Microsoft.UI.Xaml/index.json"
+	$json = Invoke-RestMethod -Method Get -Uri $url
+	# The versions are in reverse chronological order, so the first one is the latest
+	$latestVersion = $json.versions[-1]
+	return $latestVersion
 }
 
-$wingetUrl = getNewestLink("msixbundle")
-$wingetLicenseUrl = getNewestLink("License1.xml")
+# Initialize Microsoft.UI.Xaml version
+$MicrosoftUIXamlVersion = Get-LatestMicrosoftUIXamlVersion
 
-function section($text) {
+# URLs
+$urlMicrosoftUIXaml = "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/$MicrosoftUIXamlVersion"
+$urlVCLibsx64 = "https://aka.ms/Microsoft.VCLibs.x64.$VCLibsVersion.Desktop.appx"
+$urlVCLibsx86 = "https://aka.ms/Microsoft.VCLibs.x86.$VCLibsVersion.Desktop.appx"
+
+# Adding AppxPackage and silently continue on error
+function Add-AppxPackageSilently($pkg) {
 	<#
         .SYNOPSIS
-        Prints a section divider for easy reading of the output.
-
-        .DESCRIPTION
-        Prints a section divider for easy reading of the output.
-    #>
-	Write-Output "###################################"
-	Write-Output "# $text"
-	Write-Output "###################################"
-}
-
-# Add AppxPackage and silently continue on error
-function AAP($pkg) {
-	<#
-        .SYNOPSIS
-        Adds an AppxPackage to the system.
-
-        .DESCRIPTION
-        Adds an AppxPackage to the system.
+        Adds an AppxPackage to the system and silently continues on error.
+        .EXAMPLE
+        PS C:\> Add-AppxPackageSilently("https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx")
     #>
 	Add-AppxPackage $pkg -ErrorAction SilentlyContinue
 }
 
-#(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders").PSObject.Properties["Common Documents"].Value
-#Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" -Name "Common Documents"
-#(New-Object -ComObject Shell.Application).NameSpace('Shell:Common Documents').Self.Path
-#$downloadsFolder = (Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders").PSObject.Properties["{374DE290-123F-4565-9164-39C4925E467B}"].Value
-#$downloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-$downloadFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-
-# Download XAML nupkg and extract appx file
-section("Downloading Xaml nupkg file... (19000000ish bytes)")
-$url = "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8.1"
-$nupkgFolder = "$downloadFolder\Microsoft.UI.Xaml.2.8.1"
-$zipFile = "$downloadFolder\Microsoft.UI.Xaml.2.8.1.nupkg.zip"
-Invoke-WebRequest -Uri $url -OutFile $zipFile
-section("Extracting appx file from nupkg file...")
-Expand-Archive $zipFile -DestinationPath $nupkgFolder
-
-# Determine architecture
-if ([Environment]::Is64BitOperatingSystem) {
-	section("64-bit OS detected")
-
-	# Install x64 VCLibs
-	section("Downloading & installing x64 VCLibs... (21000000ish bytes)")
-	AAP("https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx")
-
-	# Install x64 XAML
-	section("Installing x64 XAML...")
-	AAP("$nupkgFolder\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx")
-} else {
-	section("32-bit OS detected")
-
-	# Install x86 VCLibs
-	section("Downloading & installing x86 VCLibs... (21000000ish bytes)")
-	AAP("https://aka.ms/Microsoft.VCLibs.x86.14.00.Desktop.appx")
-
-	# Install x86 XAML
-	section("Installing x86 XAML...")
-	AAP("$nupkgFolder\tools\AppX\x86\Release\Microsoft.UI.Xaml.2.8.appx")
+# Generates a section divider for easy reading of the output.
+function Write-Section($text) {
+	<#
+        .SYNOPSIS
+        Prints a section divider for easy reading of the output.
+        .EXAMPLE
+        PS C:\> Write-Section("Downloading Files...")
+    #>
+	Write-Output ("#" * 50)
+	Write-Output "# $text"
+	Write-Output ("#" * 50)
+	Write-Output ""
 }
 
-# Finally, install winget
-section("Downloading winget... (21000000ish bytes)")
-$wingetPath = "$downloadFolder\winget.msixbundle"
-Invoke-WebRequest -Uri $wingetUrl -OutFile $wingetPath
-$wingetLicensePath = "$downloadFolder\license1.xml"
-Invoke-WebRequest -Uri $wingetLicenseUrl -OutFile $wingetLicensePath
-section("Installing winget...")
-Add-AppxProvisionedPackage -Online -PackagePath $wingetPath -LicensePath $wingetLicensePath -ErrorAction SilentlyContinue
+function Get-NewestLink($match) {
+	<#
+    .SYNOPSIS
+        This function fetches the newest link for a specified match from the GitHub API.
+    .EXAMPLE
+        PS C:\> Get-NewestLink("msixbundle")
+    #>
+	$uri = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+	Write-Verbose "[$((Get-Date).TimeofDay)] Getting information from $uri"
+	$get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
+	Write-Verbose "[$((Get-Date).TimeofDay)] getting latest release"
+	$data = $get.assets | Where-Object name -Match $match
+	return $data.browser_download_url
+}
 
-# Adding WindowsApps directory to PATH variable for current user
-section("Adding WindowsApps directory to PATH variable for current user...")
+# Using temp directory for downloads
+$tempFolder = [System.IO.Path]::GetTempPath()
+
+# Determine architecture
+$arch = if ([Environment]::Is64BitOperatingSystem) { "x64" } else { "x86" }
+
+# Download XAML nupkg and extract appx file
+Write-Output ""
+Write-Section("Downloading Xaml nupkg file...")
+$zipFile = Join-Path -Path $tempFolder -ChildPath "Microsoft.UI.Xaml.$MicrosoftUIXamlVersion.nupkg.zip"
+Invoke-WebRequest -Uri $urlMicrosoftUIXaml -OutFile $zipFile
+$nupkgFolder = Join-Path -Path $tempFolder -ChildPath "Microsoft.UI.Xaml.$MicrosoftUIXamlVersion"
+Expand-Archive -Path $zipFile -DestinationPath $nupkgFolder -Force
+
+# Install VCLibs
+Write-Section("Downloading & installing ${arch} VCLibs...")
+$urlVCLibs = if ($arch -eq "x64") { $urlVCLibsx64 } else { $urlVCLibsx86 }
+Add-AppxPackageSilently $urlVCLibs
+
+# Install XAML
+Write-Section("Installing ${arch} XAML...")
+$XamlAppxPath = Join-Path -Path $nupkgFolder -ChildPath "tools\AppX\$arch\Release\Microsoft.UI.Xaml.2.8.appx"
+Add-AppxPackageSilently $XamlAppxPath
+
+# Finally, install winget
+$wingetUrl = Get-NewestLink("msixbundle")
+$wingetPath = Join-Path -Path $tempFolder -ChildPath "winget.msixbundle"
+$wingetLicenseUrl = Get-NewestLink("License1.xml")
+$wingetLicensePath = Join-Path -Path $tempFolder -ChildPath "license1.xml"
+Write-Section("Downloading winget...")
+Invoke-WebRequest -Uri $wingetUrl -OutFile $wingetPath
+Invoke-WebRequest -Uri $wingetLicenseUrl -OutFile $wingetLicensePath
+Write-Section("Installing winget...")
+Add-AppxProvisionedPackage -Online -PackagePath $wingetPath -LicensePath $wingetLicensePath -ErrorAction SilentlyContinue | Out-Null
+
+# Adding WindowsApps directory to PATH variable for current user if not already present
+Write-Section("Checking and adding WindowsApps directory to PATH variable for current user if not present...")
 $path = [Environment]::GetEnvironmentVariable("PATH", "User")
-$path = $path + ";" + [IO.Path]::Combine([Environment]::GetEnvironmentVariable("LOCALAPPDATA"), "Microsoft", "WindowsApps")
-[Environment]::SetEnvironmentVariable("PATH", $path, "User")
+$WindowsAppsPath = [IO.Path]::Combine([Environment]::GetEnvironmentVariable("LOCALAPPDATA"), "Microsoft", "WindowsApps")
+if (!$path.Contains($WindowsAppsPath)) {
+	$path = $path + ";" + $WindowsAppsPath
+	[Environment]::SetEnvironmentVariable("PATH", $path, "User")
+}
 
 # Cleanup
-section("Cleaning up...")
-Remove-Item $zipFile
-Remove-Item $nupkgFolder -Recurse
-Remove-Item $wingetPath
-Remove-Item $wingetLicensePath
+Write-Section("Cleaning up...")
+Remove-Item -Path $zipFile
+Remove-Item -Path $nupkgFolder -Recurse
+Remove-Item -Path $wingetPath
+Remove-Item -Path $wingetLicensePath
 
 # Finished
-section("Installation complete!")
-section("Please restart your computer to complete the installation.")
+Write-Section("Installation complete!")
+Write-Section("Please restart your computer to complete the installation.")
